d8158924d8621a18076736c79f1d2398b7e1a1ff
Jose <josebalserospinto@gmail.com>
2023-10-22 15:55:15 -0400

    Changes

--- src/Componentes/QueEsParlacheBanner/page.js
+++ src/Componentes/QueEsParlacheBanner/page.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ContenedorBusqueda from '../contenedorBusqueda/ContenedorBusqueda';
 import ChatContenedor from '../chatContenedor/ChatContenedor';
 import './QueEsParlacheEstilo.css';
 
 function QueEsParlache() {
+    const [ultimoScroll, setUltimoScroll] = useState(0);
+    const [mostrarElementos, setMostrarElementos] = useState(false);
+  
+    useEffect(() => {
+      const manejarScroll = () => {
+        const scrollActual = window.scrollY;
+  
+        if (scrollActual > ultimoScroll) {
+          // Se hizo scroll hacia abajo
+          setMostrarElementos(true);
+        } else {
+          // Se hizo scroll hacia arriba
+          setMostrarElementos(false);
+        }
+        setUltimoScroll(scrollActual);
+      };
+  
+      window.addEventListener('scroll', manejarScroll);
+      return () => {
+        window.removeEventListener('scroll', manejarScroll);
+      };
+    }, [ultimoScroll]);
     return(
         <div className= "main-parlache">
-            <h1>¿Qué más, parcero? ¿Bien o qué?</h1>
+            {mostrarElementos && <h1>¿Qué más, parcero? ¿Bien o qué?</h1>}
 
             <div className= 'introduccion-parlache'>
             <p> El parlache es una forma vernácula del español colombiano, originada principalmente en comunidades urbanas y populares. Este sociolecto se ha consolidado como un fenómeno lingüístico intrigante para sociolingüistas y antropólogos debido a su capacidad para codificar complejas realidades sociales, económicas y culturales.</p> 
 
             <p>Parlache sirve como una herramienta académica, social y antropológica para estudiar, entender y promover este dialecto colombiano.</p>
             </div>
-            <ContenedorBusqueda />
+            {mostrarElementos && <ContenedorBusqueda />}
             <ChatContenedor />
 
         </div>
--- src/Componentes/contenedorBusqueda/ContenedorBusqueda.js
+++ src/Componentes/contenedorBusqueda/ContenedorBusqueda.js
@@ -33,7 +33,7 @@ const ContenedorBusqueda = () => {
 
             <div className = 'contenedor-busqueda'>
                 <select placeholder = 'Selectores' className='campos'>
-                    <option>Aleatoia</option>
+                    <option>Aleatoria</option>
                     <option>Contiene</option>
                     <option>Empieza por</option>
                 </select>
